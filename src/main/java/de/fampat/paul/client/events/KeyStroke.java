bc5515b57200a4bcd9b4de208a6a210d9ca16240
Fampat <mail@fampat.de>
2023-01-03 18:34:09 +0100

    FIX Key titles

--- src/main/java/de/fampat/paul/client/events/KeyStroke.java
+++ src/main/java/de/fampat/paul/client/events/KeyStroke.java
@@ -16,7 +16,7 @@ public class KeyStroke {
     public static void registerPaulSpawnToggle() {
         // Setup the key
         keyBindingPaulSpawn = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "paul_come_here.paul.fampat.de",
+            "paul_come_here.text.paul.fampat.de",
             InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_P,
             "paul.paul.fampat.de"
@@ -35,7 +35,7 @@ public class KeyStroke {
     public static void registerPaulSitToggle() {
         // Setup the key
         keyBindingPaulSit = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "paul_sit.paul.fampat.de",
+            "paul_sit.text.paul.fampat.de",
             InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_O,
             "paul.paul.fampat.de"
